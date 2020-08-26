7dab9c1bc802b05c65df38f566c1a356243b1def
Adam Cooke <me@adamcooke.io>
2020-08-26 21:46:12 +0100

    test: fixes commit parser text since commits were reversed

--- spec/specs/commit_parser_spec.rb
+++ spec/specs/commit_parser_spec.rb
@@ -1,25 +1,26 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'git'
 require 'schmersion/commit_parser'
 
 describe Schmersion::CommitParser do
   context '#parse' do
     describe 'parses all commits from all time' do
       before(:all) do
-        @parser = described_class.new(EXAMPLE_REPO_PATH, :start, 'cd23926fbd3d44c806ae0e7e1891de25edfa6406')
+        @parser = described_class.new(Git.open(EXAMPLE_REPO_PATH), :start, 'cd23926fbd3d44c806ae0e7e1891de25edfa6406')
       end
 
       specify 'containing all commits' do
-        expect(@parser.commits.size).to eq 20
+        expect(@parser.commits.size).to eq 19
       end
 
       specify 'containing a latest commit with the correct name' do
-        expect(@parser.commits.first.message.header).to eq 'docs(readme): add dev details to readme'
+        expect(@parser.commits.first.message.header).to eq 'feat: adds widget management'
       end
 
       specify 'containing the oldest commit with the correct message' do
-        expect(@parser.commits.last.message.header).to eq 'feat: adds widget management'
+        expect(@parser.commits.last.message.header).to eq 'docs(readme): add dev details to readme'
       end
     end
   end
